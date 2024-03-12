b4c61939382ebf74db41cca77d3c8dd51150bbd1
HZreal <hzreal0823@gmail.com>
2024-03-13 00:53:09 +0800

    feat: introduce ant-design-vue@3.2.20

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index-page">
+    <a-input-search
+      v-model:value="value"
+      placeholder="input search text"
+      enter-button
+      @search="onSearch"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const searchText = ref("");
+const onSearch = (value: string) => {
+  alert(value);
+};
+</script>
+
+<style scoped></style>
